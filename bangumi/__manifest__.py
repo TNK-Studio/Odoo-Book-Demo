2bc6a69c9318d23532a8a134231cfa4fb9a9272e
Gzp_ <741424975@qq.com>
2019-01-11 16:55:35 +0800

    [MOD] modify bangumi/__manifest__.py

--- bangumi/__manifest__.py
+++ bangumi/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "bangumi",
-
+    'name': "Bangumi",
+    'application': True,
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
