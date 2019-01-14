1b1dd4f25fc6b11335a832b0de07b8c5966b8477
Gzp_ <741424975@qq.com>
2019-01-14 20:54:51 +0800

    [ADD] add Bangumi model

---
+++ bangumi/models/bangumi.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'gzp'
+
+from odoo import models, fields, api
+
+
+class Bangumi(models.Model):
+    _name = 'bangumi.bangumi'
+    _description = 'Bangumi'
+
+    name = fields.Char(string='Name', required=True)
+    total = fields.Integer(string='Total', required=True)
+    already_seen = fields.Integer(string='Already seen', default=0)
+    score = fields.Float(string='Score', required=True, default=0.0)
