c76212e75a497bcab735673e33abffa974f41284
Gzp_ <741424975@qq.com>
2019-01-16 22:43:21 +0800

    [ADD] add current compute field to bangumi

--- bangumi/models/bangumi.py
+++ bangumi/models/bangumi.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'gzp'
 
+import math
+
 from odoo import models, fields, api
 
 
@@ -8,7 +10,31 @@ class Bangumi(models.Model):
     _name = 'bangumi.bangumi'
     _description = 'Bangumi'
 
+    @api.multi
+    @api.depends('release_date', 'update_cycle', 'total')
+    def _compute_current(self):
+        for record in self:
+            today = fields.Date.today()
+            dt = today - record.release_date
+            if dt.days < 0:
+                self.current = 0
+                continue
+
+            if record.update_cycle == 'weekly':
+                current = round(dt.days / 7)
+                record.current = current if current < record.total else record.total
+
+            if record.update_cycle == 'monthly':
+                month_dt = (today.year - record.release_date.year) * 12 + (today.month - record.release_date.month)
+                record.current = month_dt if month_dt < record.total else record.total
+
+            if record.update_cycle == 'quarterly':
+                quarter_dt = (today.year - record.release_date.year) * 4 + (
+                        math.ceil(today.month / 3) - math.ceil(record.release_date.month / 3))
+                record.current = quarter_dt if quarter_dt < record.total else record.total
+
     name = fields.Char(string='Name', required=True)
+    current = fields.Integer(string='Current', compute='_compute_current')
     total = fields.Integer(string='Total', required=True)
     already_seen = fields.Integer(string='Already seen', default=0)
     score = fields.Float(string='Score', required=True, default=0.0)
@@ -22,3 +48,11 @@ class Bangumi(models.Model):
         'bangumi_id', 'tag_id',
         string='Bangumi Tags'
     )
+
+    update_cycle = fields.Selection([
+        ('weekly', 'Update weekly'),
+        ('monthly', 'Update monthly'),
+        ('quarterly', 'Update quarterly')
+    ], string='Update cycle', required=True, default='weekly')
+
+    release_date = fields.Date(string='Release date', default=fields.Date.today(), required=True)
