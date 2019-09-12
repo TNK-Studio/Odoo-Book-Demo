ff37d43719b37fbb35f1da9e29377af7ae2c6437
elfgzp <741424975@qq.com>
2019-09-12 10:57:55 +0800

    [Add] menu

--- bangumi/__manifest__.py
+++ bangumi/__manifest__.py
@@ -24,9 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menus.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
